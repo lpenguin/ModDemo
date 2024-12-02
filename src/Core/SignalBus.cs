33174a55b603f91718ab61f717b02c7ad1ebfc16
Nikita Prianichnikov <lpenguin@yandex.ru>
2024-12-02 17:15:43 +0100

    SignalBus

---
+++ src/Core/SignalBus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Godot;
+
+namespace ModDemo.Core;
+
+[GlobalClass]
+public partial class SignalBus : Node
+{
+    private static SignalBus? _instance;
+
+    public static SignalBus Instance => _instance ?? throw new NullReferenceException("SignalBus instance is not initialized.");
+
+    public SignalBus()
+    {
+        _instance = this;
+    }
+
+    [Signal]
+    public delegate void SignalDispatchedEventHandler(string signalName, Variant args);
+
+    public static void DispatchSignal(string signalName, Variant args)
+    {
+        Instance.EmitSignal(SignalName.SignalDispatched, signalName, args);
+    }
+}
