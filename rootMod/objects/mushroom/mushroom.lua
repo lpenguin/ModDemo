983db4b19c91926c71a6cf951d033f74e8255f52
Nikita Prianichnikov <lpenguin@yandex.ru>
2024-12-06 22:20:43 +0100

    Scripts

--- rootMod/objects/mushroom/mushroom.lua
+++ rootMod/objects/mushroom/mushroom.lua
@@ -1,13 +1,17 @@
+local isDestroyed = false
 function Ready() 
     DebugPrint("Ready")
 end
 
 function OnDamage()
-    local mushrooms = GetValue("mushrooms")
-    mushrooms = mushrooms + 1
-    SetValue("mushrooms", mushrooms)
-    local rid = GetScriptParent()
-    local position = GetObjectPosition(rid)
-    PlayEffect("MushroomExplosion", position)
-    DestroyObject(rid)
+    if not isDestroyed then
+        isDestroyed = true
+        local mushrooms = GetValue("mushrooms")
+        mushrooms = mushrooms + 1
+        SetValue("mushrooms", mushrooms)
+        local rid = GetScriptParent()
+        local position = GetObjectPosition(rid)
+        PlayEffect("MushroomExplosion", position)
+        DestroyObject(rid)
+    end
 end 
